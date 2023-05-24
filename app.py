7675b1307455902df2af41395ec233176bc67c2c
Kunal-inno <sakshi_gupta@innotechteam.in>
2023-05-24 19:43:56 +0530

    flask-app

---
+++ app.py
@@ -0,0 +1,117 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re  
+
+app = Flask(__name__) 
+
+app.secret_key = 'xyzsdfg'
+  
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'main-system'
+  
+mysql = MySQL(app)  
+
+@app.route('/')
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    mesage = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s AND password = % s', (email, password))
+        user = cursor.fetchone()
+        if user:            
+            session['loggedin'] = True
+            session['userid'] = user['userid']
+            session['name'] = user['name']
+            session['email'] = user['email']
+            mesage = 'Logged in successfully !'
+            return redirect(url_for('users'))
+        else:
+            mesage = 'Please enter correct email / password !'
+    return render_template('login.html', mesage = mesage)
+  
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('userid', None)
+    session.pop('email', None)
+    session.pop('name',None)
+    return redirect(url_for('login'))
+
+@app.route("/users", methods = ['GET', 'POST'])
+def users():
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user')
+        users = cursor.fetchall()
+        return render_template("users.html", users = users)
+    return redirect(url_for('login'))
+
+@app.route("/view", methods =['GET', 'POST'])
+def view():
+    if 'loggedin' in session:
+        viewUserId = request.args.get('userid')   
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE userid = % s', (viewUserId, ))
+        user = cursor.fetchone()   
+        return render_template("view.html", user = user)
+    return redirect(url_for('login'))
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    mesage = ''
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form :
+        userName = request.form['name']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s', (email, ))
+        account = cursor.fetchone()
+        if account:
+            mesage = 'User already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            mesage = 'Invalid email address !'
+        elif not userName or not password or not email:
+            mesage = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO user VALUES (NULL, % s, % s, % s)', (userName, email, password))
+            mysql.connection.commit()
+            mesage = 'New user created!'
+    elif request.method == 'POST':
+        mesage = 'Please fill out the form !'
+    return render_template('register.html', mesage = mesage)
+
+
+@app.route("/edit", methods =['GET', 'POST'])
+def edit():
+    msg = ''    
+    if 'loggedin' in session:
+        editUserId = request.args.get('userid')
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE userid = % s', (editUserId, ))
+        editUser = cursor.fetchone()
+        if request.method == 'POST' and 'name' in request.form and 'userid' in request.form  :
+            userName = request.form['name']             
+            userId = request.form['userid']
+            if not re.match(r'[A-Za-z0-9]+', userName):
+                msg = 'name must contain only characters and numbers !'
+            else:
+                cursor.execute('UPDATE user SET  name =% s, WHERE userid =% s', (userName,  (userId, ), ))
+                mysql.connection.commit()
+                msg = 'User updated !'
+                return redirect(url_for('users'))
+        elif request.method == 'POST':
+            msg = 'Please fill out the form !'        
+        return render_template("edit.html", msg = msg, editUser = editUser)
+    return redirect(url_for('login'))
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
